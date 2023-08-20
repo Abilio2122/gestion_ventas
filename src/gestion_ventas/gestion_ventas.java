4ddb412ce0e8998f01f0bc90cbab5eee6b89bd1d
Abilio2122 <ruben.carmun@gmail.com>
2023-08-20 18:19:53 -0400

    primer commit

---
+++ src/gestion_ventas/gestion_ventas.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gestion_ventas;
+
+/**
+ * 
+ */
+public class gestion_ventas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
