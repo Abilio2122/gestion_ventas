24e5f537e66cb377260dbda8aebab24a5e50b4b0
davlobos <carlos.dasilva.v@mail.pucv.cl>
2023-09-07 18:47:46 -0300

    Se eliminó función mstrar metodo pago , y agregar metodo pago por error de mapeo de codigo.

--- src/gestion_ventas/gestionPago.java
+++ src/gestion_ventas/gestionPago.java
@@ -10,23 +10,13 @@ public class gestionPago {
     	listaPago = new ArrayList<Pago>();
     }
     
-    public void agregarMetPago(Pago metPago) {
-    	listaPago.add(metPago);
+    public void agregarInfoPago(Pago infoPago) {
+    	listaPago.add(infoPago);
     }
 
-    public void tiposPago() {
-        gestionPago gestion = new gestionPago();
-        gestion.agregarMetPago(new Pago("Tarjeta de Crédito <(0)>"));
-        gestion.agregarMetPago(new Pago("Tarjeta debito <(1)>"));
-        gestion.agregarMetPago(new Pago("WebPay <(2)>"));
-        gestion.agregarMetPago(new Pago("Mercado Pago <(3)>"));
-        gestion.agregarMetPago(new Pago("Transferencia Bancaria <(4)>"));
-    }
 
-	public void mostrarMetodosPago() {
-        System.out.println("Métodos de Pago Disponibles:");
-        for (Pago pago : listaPago) {
-            System.out.println(pago);
-        }
-    }
+    
+    //Hay que ver funcion mostrarMetodosPago()
+
+    
 }
